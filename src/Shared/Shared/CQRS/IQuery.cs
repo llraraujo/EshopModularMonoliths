8e35d3a80f8492b3f8cddd0a96d38c0f2852b871
llraraujo <lucasregoaraujo@gmail.com>
2025-08-05 16:14:37 -0300

    Setting up MediatR interfaces and Created the CreateProductCommandHandler class

---
+++ src/Shared/Shared/CQRS/IQuery.cs
@@ -0,0 +1,14 @@
+﻿
+using MediatR;
+
+namespace Shared.CQRS
+{
+
+    public interface IQuery: IQuery<Unit>
+    {
+    }
+
+    public interface IQuery<out TResponse>: IRequest<TResponse>
+    {
+    }
+}
